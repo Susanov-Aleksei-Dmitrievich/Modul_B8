4460b6987b6cb7120c1bb4ec3b9472042420cc4b
Susanov-Aleksei-Dmitrievich <AlexGit26@yandex.ru>
2024-05-11 11:56:44 +0300

    Программа. Исправленный вариант № 1. Нужно выполнять проверку этого проекта.

--- release/UchetOborudovania/UchetOborudovania/Forms/OknoAvtorizacia.Designer.cs
+++ release/UchetOborudovania/UchetOborudovania/Forms/OknoAvtorizacia.Designer.cs
@@ -116,6 +116,7 @@
             this.button2.TabIndex = 8;
             this.button2.Text = "Окно. Сотрудник";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
@@ -126,6 +127,7 @@
             this.button3.TabIndex = 9;
             this.button3.Text = "Окно. Руководитель";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
@@ -136,6 +138,7 @@
             this.button4.TabIndex = 10;
             this.button4.Text = "Окно. Администратор";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // checkBoxMaska
